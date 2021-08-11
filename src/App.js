9483c6543f016c944c0bb228455a11be7e0a9e82
Noe Gonzalez <palmas.inter@gmail.com>
2021-08-10 17:04:48 -0700

    RUTAS PROTEGIDAS

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import { GlobalStyle } from './styles/GlobalStyles'
 import { Router } from '@reach/router'
 import { Detail } from './pages/Detail'
 import { NavBar } from './components/NavBar'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 
 export const App = () => {
+
+  const UserLogged = ({children}) => {
+    return children({isAuth: false})
+  }
 
   return (
     <>
@@ -16,7 +23,22 @@ export const App = () => {
           <Home path='/' />
           <Home path='/pet/:id' />
           <Detail path='/detail/:detailId' />
-        </Router>
+      </Router>
+      <UserLogged>
+        {
+          ({isAuth}) => 
+          isAuth 
+          ? <Router>
+              <Favs path='/favs' />
+              <User path='/user' />
+          </Router>
+          
+          : <Router>
+            <NotRegisteredUser path='/favs' />
+            <NotRegisteredUser path='/user' />
+          </Router>
+        }
+      </UserLogged>
         <NavBar /> 
     </>
   )
---
+++ src/pages/NotRegisteredUser.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NotRegisteredUser = () => {
+    return (
+        <div>
+            NotRegisteredUser
+        </div>
+    )
+}
