8f4f2c80e7366bf637c3e842c89b4a47e90acc7b
Noe Gonzalez <palmas.inter@gmail.com>
2021-09-11 10:22:23 -0700

    PWA MANIFEST, SERVICE WORKER

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+
+const path = require('path')
 
 module.exports = {
   devtool: 'source-map',
@@ -8,7 +12,38 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      template: 'public/index.html'
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petgram - Tu app de ftoos de mascotas',
+      shortname: 'Petgram',
+      description: 'Con Petgram puedes encontrar fotos de animales domésticos my fácilmente',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com|images.unsplash.com)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://petgram-server.midudev.now.sh'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
+        }
+      ]
     })
   ],
   module: {
